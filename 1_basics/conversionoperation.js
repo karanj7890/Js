58498a49bb4ef74e925b3b2c5e61c0a1cc457df9
karanj7890 <124456783+karanj7890@users.noreply.github.com>
2023-10-15 14:05:21 +0000

    conversion

--- 1_basics/conversionoperation.js
+++ 1_basics/conversionoperation.js
@@ -26,5 +26,38 @@ let BoolisLoggedIn= Boolean(isloggedIn);
 
 let num= 33;
 let str= String(num);
-console.log(str);
-console.log(typeof str);
+// console.log(str);
+// console.log(typeof str);
+
+//***************************Operations *********************************/
+let value=5
+let negvalue= -value
+//console.log(negvalue);
+
+//basic maths operations like addition,sub,mul,div,power etc
+let str1= "karan"
+let str2= " jalal"
+let str3= str1 + str2
+//console.log(str3);
+
+// console.log(1+"2");
+// console.log(1+"2" + "2");
+// console.log(1 + 2 +"2");
+// console.log("1" + 2 + 2 );
+
+//console.log(+true);
+
+let game= 100;
+++game
+// console.log(game);
+
+//prefix and postfix increment
+let x=3
+const y= x++
+console.log(y);
+// here x will be 4 and y will be 3
+
+let i= 5
+const j= ++i
+console.log(j);
+// here i will be 6 and j will be 6
