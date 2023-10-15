900f911ff68c6ee8d39d3042880e89cedcc6c527
karanj7890 <124456783+karanj7890@users.noreply.github.com>
2023-10-15 11:32:45 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using nodejs,not browser
+
+//code readibilty is important
+let name= "Karan"
+let age= 18
+//number=> 2 to power 53
+//bigint
+//string=> ""
+//boolean=> true,false
+//null=>standalone value
+//undefined=> variable has been declared but not initialised with value
+//symbol=>unique
+
+
+//object=>
+
+console.log(typeof null);
