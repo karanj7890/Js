030e84e989899603672feaf60ae1121d497be455
karanj7890 <124456783+karanj7890@users.noreply.github.com>
2023-10-15 11:15:43 +0000

    config done

---
+++ 1_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=75797
+let accountEmail= "karan@gmail.com"
+var accountPassword= "12345"
+accountCity= "Jaipur"
+
+// accountId= 3552  not allowed
+accountEmail="jaode@gmail.com"
+accountPassword= "6626"
+accountCity="kanpur"
+let accountState;
+/*
+Preffered using let variable in js,not use var variable as it doesn't work on scope and functional scope
+*/
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
