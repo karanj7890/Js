c8c436f60db105d6b9fc0bd3b970e29c27cb552b
karanj7890 <124456783+karanj7890@users.noreply.github.com>
2023-10-15 16:18:56 +0000

    comp done

---
+++ 1_basics/comparison.js
@@ -0,0 +1,9 @@
+// console.log("2" > 1);
+// console.log(2 > "1");
+
+// console.log(null > 0);
+// console.log(null==0);
+// console.log(null >= 0);  //null is converted to zero as comparision operator is used
+console.log(undefined==0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
