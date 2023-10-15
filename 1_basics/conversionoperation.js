ff774c7414318de45bf92418bc79cf5e4113d44c
karanj7890 <124456783+karanj7890@users.noreply.github.com>
2023-10-15 11:59:33 +0000

    coversiondone

---
+++ 1_basics/conversionoperation.js
@@ -0,0 +1,30 @@
+let score= undefined
+
+//console.log(typeof score);
+
+let inNumber= Number(score);
+
+//console.log(typeof inNumber);
+
+//console.log(inNumber);  //it will show NaN i.e Not A Number 
+
+/*
+"33"=> 33
+"33and"=> NaN
+true=1
+false=0
+*/
+let isloggedIn = "karan"
+
+let BoolisLoggedIn= Boolean(isloggedIn);
+//console.log(BoolisLoggedIn);
+
+// 1=>true
+// 0=>false
+//""=> false
+//"Karan"=> true
+
+let num= 33;
+let str= String(num);
+console.log(str);
+console.log(typeof str);
