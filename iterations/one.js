8313670b56a089215fec0dd8013b2597e60e206c
karanj7890 <124456783+karanj7890@users.noreply.github.com>
2023-11-05 06:52:55 +0000

    forloop

---
+++ iterations/one.js
@@ -0,0 +1,18 @@
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element==5) {
+        // console.log("5 is printed");
+    }
+    // console.log(element);
+}
+
+for (let i = 0; i <=10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <=10; j++) {
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+    }
+}
+
+// break and continue
